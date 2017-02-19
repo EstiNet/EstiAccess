442aebed174edefadd35572fb711d95e5b0deea9
espidev <aguy867@gmail.com>
2017-02-19 17:29:38 -0500

    Changingggggggggggg

--- menu.js
+++ menu.js
@@ -1,10 +1,11 @@
 var menuService = function(){
 var self = this;
     self.getLi = function (){
+        var remote = require('electron').remote; 
     console.log("Calling getLi()")
     var retur = ""
-    console.log(global.sessionArray);
-    global.sessionArray.forEach(function(element){
+    console.log(remote.getGlobal('sessionArray'));
+    remote.getGlobal('sessionArray').forEach(function(element){
         retur += '<li><a href="#">' + element.name + '</a></li>'
     });
     return retur
