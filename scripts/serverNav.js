f8763760f1e884d2eb68abe5c083eaa65e7cd694
espidev <aguy867@gmail.com>
2017-02-28 18:19:56 -0500

    Changes

--- scripts/serverNav.js
+++ scripts/serverNav.js
@@ -1,3 +1,9 @@
+function configureServer(){
+    $("#data").fadeOut(300, function(){
+       $("#container-f").hide();
+       $("#container-f").load("./html/server")
+    });
+}
 function openConsoleLog(){
     $( "#data" ).remove();
     $( "#container-f" ).load( "./html/serverset.html", function() {});
