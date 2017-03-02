a073c5894d5a55b999318fc57d6e3625c8755bfe
espidev <aguy867@gmail.com>
2017-03-01 21:31:18 -0500

    Changes

--- storage.js
+++ storage.js
@@ -1,3 +1,4 @@
+var jsonfile = require("jsonfile");
 const expor = module.exports = {};
 expor.indexStorage = function () {
     const util = require('./vars.js');
@@ -10,22 +11,33 @@ expor.indexStorage = function () {
     }
     fs.readdir(dir, (err, files) => {
         files.forEach(file => {
+            file = "./sessions/" + file;
             fs = require('fs');
-            fs.readFile(file, 'utf8', function (err, data) {
+            const jsonFile = require('jsonfile');
+            jsonfile.readFile(file, function(err, obj) {
                 if (err) {
                     return console.log(err);
                 }
-                var ob = JSON.parse(data);
-                util.configureArray.push(ob)
+                util.configureArray.push(obj)
             });
         });
     });
 };
 
 expor.createSession = function (name, ip, port, password) {
+    const jsonFile = require('jsonfile');
     const util = require('./vars.js');
-    var json = {'name': name, 'ip': ip, 'port': port, 'password': password, 'socketid': -1};
     var fs = require('fs');
-    fs.writeFile("sessions/" + name + ".json", json);
-    util.sessionArray.push(json)
+    var dir = "sessions/" + name + ".json";
+    if(!fs.existsSync(dir)){
+        fs.openSync(dir, 'a');
+    }
+    var json = {'name': name, 'ip': ip, 'port': port, 'password': password};
+    jsonFile.writeFile(dir, json, function (err) {
+        if(err){
+            console.error(err)
+        }
+    });
+    util.configureArray.push(json);
+    util.startSocket(json);
 };
